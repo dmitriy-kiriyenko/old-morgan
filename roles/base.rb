f2320347b5e52ea72575ea6faebffa282886017e
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-09-20 14:40:53 +0300

    Provide dak public key.

--- roles/base.rb
+++ roles/base.rb
@@ -5,12 +5,12 @@ default_attributes maintenance: {
                      deploy_user: {
                        name: 'deploy',
                        group: 'deploy',
-                       ssh_key: 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAypn5krEiWWykcLeH7QvYUCmMLr1TYsFxDfh57OvkeOdMKubhbNFDfcFiQ0e4LuuSPbv3xLbClfDZa//O/MfXrZ0HIGIb2MWhw22Nrgobrtr+qt26m/Su+z0cyGYf0e6I6QuAiEEPhl9Ge75HfuWlwtmmiPcaB9YHu2+DWdSTvXM='
+                       ssh_key: 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEA+AwyvE4+LKS/0jU3abhD4Hpiu9pYTOeJjIPX5r2pOC/mh6I5zkFsrVZDfmZgKJwzRc4Vpj9W5dwvk8tArgNlQ7pXff+SAdTeE9piTn+9j+jhaBYzHKNlBXFRGT0o3huj2YA+SAIPGm5+egW0tBP2QyMQxjhP5EvUO8lkHvoYsjM= /Users/dak/.ssh/identity'
                      },
                      admin_users: [
                        {
-                         name: 'ia',
-                         ssh_key: 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAypn5krEiWWykcLeH7QvYUCmMLr1TYsFxDfh57OvkeOdMKubhbNFDfcFiQ0e4LuuSPbv3xLbClfDZa//O/MfXrZ0HIGIb2MWhw22Nrgobrtr+qt26m/Su+z0cyGYf0e6I6QuAiEEPhl9Ge75HfuWlwtmmiPcaB9YHu2+DWdSTvXM='
+                         name: 'dak',
+                         ssh_key: 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEA+AwyvE4+LKS/0jU3abhD4Hpiu9pYTOeJjIPX5r2pOC/mh6I5zkFsrVZDfmZgKJwzRc4Vpj9W5dwvk8tArgNlQ7pXff+SAdTeE9piTn+9j+jhaBYzHKNlBXFRGT0o3huj2YA+SAIPGm5+egW0tBP2QyMQxjhP5EvUO8lkHvoYsjM= /Users/dak/.ssh/identity'
                        }
                      ]
                    }

92e765523a350341185d8f3e2243482b2fa7cab7
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-09-21 14:53:34 +0300

    Don't run service.
    Also temporarily move attributes back to recipe.

--- cookbooks/redtape_application/recipes/setup_unicorn.rb
+++ cookbooks/redtape_application/recipes/setup_unicorn.rb
@@ -1,11 +1,26 @@
-node["unicorn"]["installs"] = [{ "app_root" => "/var/www/apps/redtape/current" }]
-node["unicorn"]["config"] = {
-  "path" => "/etc/unicorn/#{node["redtape_application"]["name"]}_config.rb"
+# Attributes
+app_root = '/var/www/apps/redtape/current'
+
+node['unicorn'] = {
+  'installs'  => [ { 'app_root' => app_root } ],
+
+  'config_directory' =>  '/etc/unicorn',
+  'config' => {
+    'path' => "#{node['unicorn']['config_directory']}/#{node['redtape_application']['name']}_config.rb",
+    'working_directory' => app_root,
+    'listen' => [['8080', '{ :tcp_nodelay => true, :tries => 5 }']],
+    'worker_processes' => [node['cpu']['total'].to_i * 4, 8].min
+  },
+
+  'service' => "unicorn-#{node['redtape_application']['name']}-#{node['unicorn']['rack_env']}",
+  'command' => "cd #{node['unicorn']['app_root']} && bundle exec unicorn -D -E #{node['unicorn']['rack_env']} -c #{node['unicorn']['config']['path']}",
+  'run_service' => false
 }
 
-directory "/etc/unicorn" do
-  owner "deploy"
-  group "deploy"
+# Actions
+directory node['unicorn']['config_directory'] do
+  owner "deploy" # these attributes should be taken
+  group "deploy" # from 'maintenance/attributes/default.rb'
   group 700
   recursive true
 end

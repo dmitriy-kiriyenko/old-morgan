490b3c5256de38bcfdb82e453d1751012a5dd920
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-09-21 13:14:07 +0300

    Depend on a correct cookbook.

--- cookbooks/redtape_application/metadata.rb
+++ cookbooks/redtape_application/metadata.rb
@@ -5,4 +5,4 @@ description      "Manages deployment preparations for RoR application"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "chef-unicorn"
+depends "unicorn"

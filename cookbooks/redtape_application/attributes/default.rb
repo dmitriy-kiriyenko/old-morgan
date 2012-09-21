31ec079c60d3fa7792f8d60cc1d61ab52c1f8d1b
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-09-21 13:28:20 +0300

    Keep unicorn config in etc/unicorn.

--- cookbooks/redtape_application/attributes/default.rb
+++ cookbooks/redtape_application/attributes/default.rb
@@ -1,8 +1,1 @@
-default['redtape_application']['environment'] = 'production'
-default['redtape_application']['revision']    = 'slim-ruby19'
-
-default['redtape_application']['database']['adapter']  = 'mysql2'
-default['redtape_application']['database']['database'] = 'redtape'
-default['redtape_application']['database']['host']     = 'localhost'
-default['redtape_application']['database']['username'] = 'root'
-default['redtape_application']['database']['password'] = node['mysql']['server_root_password']
+default['redtape_application']['name'] = 'redtape'
--- cookbooks/redtape_application/recipes/setup_unicorn.rb
+++ cookbooks/redtape_application/recipes/setup_unicorn.rb
@@ -1,3 +1,13 @@
 node["unicorn"]["installs"] = [{ "app_root" => "/var/www/apps/redtape/current" }]
+node["unicorn"]["config"] = {
+  "path" => "/etc/unicorn/#{node["redtape_application"]["name"]}_config.rb"
+}
+
+directory "/etc/unicorn" do
+  owner "deploy"
+  group "deploy"
+  group 700
+  recursive true
+end
 
 include_recipe "unicorn"

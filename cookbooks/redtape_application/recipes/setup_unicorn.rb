9bdb5c3f4b4dd7cdb5a88e907b1a101aab2be8b6
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-09-21 11:44:21 +0300

    Call unicorn recipe.

--- cookbooks/redtape_application/recipes/setup_unicorn.rb
+++ cookbooks/redtape_application/recipes/setup_unicorn.rb
@@ -1,5 +1,3 @@
-deploy_user  = node['maintenance']['deploy_user']['name']
-deploy_group = node['maintenance']['deploy_user']['group']
-database_params = node['redtape_application']['database']
-
 node["unicorn"]["installs"] = [{ "app_root" => "/var/www/apps/redtape/current" }]
+
+include_recipe "unicorn"

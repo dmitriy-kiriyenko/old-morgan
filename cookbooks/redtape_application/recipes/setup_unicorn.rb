f206991fe661b562cbd4ff9d163d39de21dd49ba
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-09-24 11:06:42 +0300

    Use deploy user.

--- cookbooks/redtape_application/recipes/setup_unicorn.rb
+++ cookbooks/redtape_application/recipes/setup_unicorn.rb
@@ -1,7 +1,9 @@
 node['unicorn'] = {
   'installs'  => [ {
     'app_root' => '/var/www/apps/redtape/current',
-    'run_service' => false
+    'run_service' => false,
+    'user' => 'deploy', # These variables should be detected
+    'group' => 'deploy' # from maintenance/users.rb
   } ]
 }
 

5a0a8ad600892c6c443a98daf5bade127e5bd43d
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-09-25 10:23:04 +0300

    Correct folder for pids.

--- cookbooks/redtape_application/recipes/setup_unicorn.rb
+++ cookbooks/redtape_application/recipes/setup_unicorn.rb
@@ -3,7 +3,8 @@ node['unicorn'] = {
     'app_root' => '/var/www/apps/redtape/current',
     'run_service' => false,
     'user' => node['maintenance']['deploy_user']['name'],
-    'group' => node['maintenance']['deploy_user']['group']
+    'group' => node['maintenance']['deploy_user']['group'],
+    'pid' => "#{node['unicorn']['app_root']}/pids/unicorn.pid"
   } ]
 }
 

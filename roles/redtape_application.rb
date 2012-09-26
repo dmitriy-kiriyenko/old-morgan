96e8ef8329436390a86f17665f10f385e4eef6ca
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-09-26 17:32:53 +0300

    Remove :shit:

--- roles/redtape_application.rb
+++ roles/redtape_application.rb
@@ -1,10 +1,6 @@
 name 'redtape_application'
 description 'Manages deployment of Redtape Ruby on Rails application'
 
-default_attributes copycopter: {
-                     environment: 'production',
-                   }
-
 run_list 'role[base]',
          'role[ntp]',
          'role[chef-server]',
